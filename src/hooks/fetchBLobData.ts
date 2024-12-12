1d81ec3f3f304394c946f9f0e1861e57ae48c831
Veli-Matti Heino <vekkis@gmail.com>
2024-12-12 11:20:54 +0200

    remove cache...

--- src/hooks/fetchBLobData.ts
+++ src/hooks/fetchBLobData.ts
@@ -16,7 +16,7 @@ export async function getBLOBData(): Promise<BLOB[] | { error: string }> {
   try {
     const response = await fetch(
       "https://yxkilu3yp1tkxpeo.public.blob.vercel-storage.com/data/mydata.json",
-      { next: { revalidate: 3600 } },
+      { cache: "no-store" },
     );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
